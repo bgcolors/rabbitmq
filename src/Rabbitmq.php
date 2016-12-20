b19ca15b9c46e7af1eb277712e15566c0692de6b
bgcolor <493046077@qq.com>
2016-12-20 17:42:07 +0800

    fix bugs that using seprate channel when calling instance()

--- src/Rabbitmq.php
+++ src/Rabbitmq.php
@@ -8,12 +8,12 @@ class Rabbitmq {
 
     public static function getInstance() {
         if (self::$instance) {
-            return self::$instance;
+            return self::$instance->channel();
         }
 
         $config = require_once __DIR__.'/config.php';
-        self::$instance = (new AMQPStreamConnection($config['host'], $config['port'], $config['username'], $config['password']))->channel();
+        self::$instance = (new AMQPStreamConnection($config['host'], $config['port'], $config['username'], $config['password']));
 
-        return self::$instance;
+        return self::$instance->channel();
     }
 }
